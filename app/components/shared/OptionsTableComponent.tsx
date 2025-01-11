82b361856998e0e1d50feb54df3d7b66dbec7969
ak <kothari.atit@gmail.com>
2025-01-11 13:12:54 -0800

    change button text to alert

--- app/components/shared/OptionsTableComponent.tsx
+++ app/components/shared/OptionsTableComponent.tsx
@@ -115,10 +115,10 @@ export function OptionsTableComponent({ option }: OptionsTableComponentProps) {
     setSearchCount(newCount);
     localStorage.setItem('searchCount', newCount.toString());
     
-    if (newCount >= 3) {
-      setShowSaveModal(true);
-      setSearchCount(0)
-    }
+    // if (newCount >= 3) {
+    //   setShowSaveModal(true);
+    //   setSearchCount(0)
+    // }
 
     setActiveFilters({
       searchTerm,
@@ -379,7 +379,7 @@ export function OptionsTableComponent({ option }: OptionsTableComponentProps) {
             className="text-gray-600 hover:text-gray-800 w-full md:w-auto"
           >
             <Save className="h-4 w-4 mr-2" />
-            Save Query
+            Alert
           </Button>
 
           <Button 
