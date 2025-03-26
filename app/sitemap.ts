bf5b569fe550463a999569d18445e75a8d735745
ak <kothari.atit@gmail.com>
2025-03-25 20:59:05 -0700

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -23,6 +23,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: `${baseUrl}/discover`,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.7,
+    },
+    {
       url: `${baseUrl}/options`,
       lastModified: new Date(),
       changeFrequency: 'daily',
