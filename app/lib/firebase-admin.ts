75b6c6d74d5bf693e93cfbba105e8e11b15d7317
ak <kothari.atit@gmail.com>
2025-04-17 18:25:10 -0700

    bug fix

--- app/lib/firebase-admin.ts
+++ app/lib/firebase-admin.ts
@@ -8,15 +8,15 @@ function initializeFirebaseAdmin() {
   
   if (!apps.length) {
     // Check if all required environment variables are present
-    if (!process.env.FIREBASE_PROJECT_ID || !process.env.FIREBASE_CLIENT_EMAIL || !process.env.FIREBASE_PRIVATE_KEY) {
-      throw new Error('Missing required Firebase Admin environment variables');
-    }
+    //if (!process.env.FIREBASE_PROJECT_ID || !process.env.FIREBASE_CLIENT_EMAIL || !process.env.FIREBASE_PRIVATE_KEY) {
+    //  throw new Error('Missing required Firebase Admin environment variables');
+    //}
 
     // Use environment variables for service account credentials
     const serviceAccount = {
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n') ?? '',
     };
 
     console.log('Private Key:', process.env.FIREBASE_PRIVATE_KEY);
