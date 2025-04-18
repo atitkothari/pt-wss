a6943c97e48095bfd2e125d1d132ece358b2a47a
ak <kothari.atit@gmail.com>
2025-04-17 18:28:51 -0700

    bug fix

--- app/lib/firebase-admin.ts
+++ app/lib/firebase-admin.ts
@@ -14,8 +14,8 @@ function initializeFirebaseAdmin() {
 
     // Use environment variables for service account credentials
     const serviceAccount = {
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      projectId: process.env.FIREBASE_PROJECT_ID??'',
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL??'',
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n') ?? '',
     };
 
