9e3b28e6015536be21bd7976bf991d848577e037
ak <kothari.atit@gmail.com>
2025-08-16 17:20:35 -0700

    add mautic

--- app/layout.tsx
+++ app/layout.tsx
@@ -157,6 +157,18 @@ export default function RootLayout({
             <img height="1" width="1" style={{display: 'none'}} src="https://www.facebook.com/tr?id=1084347710126303&ev=PageView&noscript=1" />
           </noscript>
         )}
+        {hasAcceptedAnalytics() && (
+          <Script id="mautic-tracking" strategy="afterInteractive">
+            {`
+              (function(w,d,t,u,n,a,m){w['MauticTrackingObject']=n;
+                  w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)},a=d.createElement(t),
+                  m=d.getElementsByTagName(t)[0];a.async=1;a.src=u;m.parentNode.insertBefore(a,m)
+              })(window,document,'script','http://mautic-jkkk408cw4gcswsgo8k84k8o.194.195.92.250.sslip.io/mtc.js','mt');
+
+              mt('send', 'pageview');
+            `}
+          </Script>
+        )}
       </body>
     </html>
   );
