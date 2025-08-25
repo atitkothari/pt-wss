f5b973061dbcb53f6aea1c115ca64eab76c3b2a0
ak <kothari.atit@gmail.com>
2025-08-25 14:46:23 -0700

    cache setting update

--- next.config.js
+++ next.config.js
@@ -51,7 +51,7 @@ const nextConfig = {
           },
           {
             key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
+            value: "public, max-age=0, must-revalidate",
           },
         ],
       },
